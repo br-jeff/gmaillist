590e016081816026977ff13b079e7413da6e7c4d
jefferson <jefferson_1996Th@hotmail.com>
2020-01-29 21:54:00 -0200

    Data Class

--- app/src/main/java/learm/com/gmaillist/Data/Data.kt
+++ app/src/main/java/learm/com/gmaillist/Data/Data.kt
@@ -1,13 +1,13 @@
 package learm.com.gmaillist.Data
 
-data class email {
+data class Email {
     val user: String,
     val subject : String,
     val preview: String,
     val date : String,
     val starread : Boolean,
     val unread: Boolean,
-    val selected : Boolean = false
+    var selected : Boolean = false
 
 }
 
@@ -21,4 +21,32 @@ class EmailBuilder {
     var unread: Boolean=  false
     var selected : Boolean = false
 
+    fun build() : Email  = Email(user,subject,preview,date,starread,unread,false)
+
 }
+
+fun email(block : EmailBuilder.() -> Unit) : Email = EmailBuilder().apply(block).build()
+
+fun fakeEmails = listOf (
+    email {
+    user = "Aline"
+    subject = "olha meu app aline"
+    preview = "te amo muito"
+    date = "12 dez"
+    starread = true
+    unread = false
+    selected = false
+    },
+        email {
+            user = "outro"
+            subject = "olha meu app aline"
+            preview = "te amo muito"
+            date = "12 dez"
+            starread = false
+            unread = false
+            selected = false
+        }
+    )
+
+
+
