50efc16869d30dc83380d148a85f65ceafb01ac2
jefferson <jefferson_1996Th@hotmail.com>
2020-01-28 22:25:24 -0200

    Start DataClass

--- app/src/main/java/learm/com/gmaillist/Data/Data.kt
+++ app/src/main/java/learm/com/gmaillist/Data/Data.kt
@@ -1,2 +1,24 @@
 package learm.com.gmaillist.Data
 
+data class email {
+    val user: String,
+    val subject : String,
+    val preview: String,
+    val date : String,
+    val starread : Boolean,
+    val unread: Boolean,
+    val selected : Boolean = false
+
+}
+
+class EmailBuilder {
+
+    var user: String = ""
+    var subject : String= ""
+    var preview: String= ""
+    var date : String= ""
+    var starread : Boolean= false
+    var unread: Boolean=  false
+    var selected : Boolean = false
+
+}
